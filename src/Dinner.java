87d34dc0577dd77559ba9b0b25e180282166ccf0
QwertzHz <lpulley99@gmail.com>
2015-05-14 13:47:18 -0500

    comments on Dinner

--- src/Dinner.java
+++ src/Dinner.java
@@ -9,16 +9,16 @@ public class Dinner
 	public static void main(String[] args)
 	{
 		Scanner steve = new Scanner(System.in);
-		List<Table> tables = new ArrayList<Table>();
-		int numSeniors, numUnder;
+		List<Table> tables = new ArrayList<Table>(); // Contains all tables
+		int numSeniors, numUnder; // Number of seniors, number of non-seniors
 		
-		System.out.print("How many seniors? ");
+		System.out.print("How many seniors? "); // Gets numSeniors
 		numSeniors = steve.nextInt();
-		System.out.print("How many other students? ");
+		System.out.print("How many other students? "); // Gets numUnder
 		numUnder = steve.nextInt();
 		
 		System.out.println("Creating " + numSeniors + " senior tables");
-		for (int i = 0; i < numSeniors; i++)
+		for (int i = 0; i < numSeniors; i++) // Creates a table for each senior
 		{
 			tables.add(new Table(true));
 		}
@@ -26,7 +26,7 @@ public class Dinner
 		if (numUnder / 7 > tables.size())
 		{
 			System.out.println("Adding " + (numUnder / 7 - tables.size()) + " overflow tables");
-			for (int i = 0; i < (numUnder / 7) - tables.size(); i++)
+			for (int i = 0; i < (numUnder / 7) - tables.size(); i++) // Creates more tables as needed for non-seniors
 			{
 				tables.add(new Table(false));
 			}
