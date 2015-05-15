2be2cce9ec7e58647c3b54fd6930e11b8eba8865
QwertzHz <lpulley99@gmail.com>
2015-05-15 13:32:50 -0500

    Implement Person into Dinner code

--- src/Dinner.java
+++ src/Dinner.java
@@ -10,31 +10,41 @@ public class Dinner
 	{
 		Scanner steve = new Scanner(System.in);
 		Table[] tables; // Contains all tables
-		int numSeniors, numUnder; // Number of seniors, number of non-seniors
+		Person[] seniors, unders;
 		int tablesNeeded = 0; // Number of tables needed to seat everyone
 		
 		System.out.println("DINNER SEATING PROGRAM");
+		
 		System.out.print("How many seniors? "); // Gets numSeniors
-		numSeniors = steve.nextInt();
-		System.out.print("How many other students? "); // Gets numUnder
-		numUnder = steve.nextInt();
-		
-		tablesNeeded += numSeniors; // Counts a table for each senior
-		
-		if (numUnder / 7 > tablesNeeded)
+		seniors = new Person[steve.nextInt()];
+		for (int i = 0; i < seniors.length; i++)
 		{
-			tablesNeeded += (numUnder / 7) - tablesNeeded; // Counts more tables as needed for non-seniors
+			seniors[i] = new Person();
+		}
+		
+		System.out.print("How many other students? "); // Gets numUnder
+		unders = new Person[steve.nextInt()];
+		for (int i = 0; i < unders.length; i++)
+		{
+			unders[i] = new Person();
+		}
+		
+		tablesNeeded += seniors.length; // Counts a table for each senior
+		
+		if (unders.length / 7 > tablesNeeded)
+		{
+			tablesNeeded += (unders.length / 7) - tablesNeeded; // Counts more tables as needed for non-seniors
 		}
 		
 		tables = new Table[tablesNeeded];
 		for (int i = 0; i < tables.length; i++)
 		{
-			if (i < numSeniors) tables[i] = new Table(true); else tables[i] = new Table(false);
+			if (i < unders.length) tables[i] = new Table(true); else tables[i] = new Table(false);
 		}
 	}
 	
-	public Person[][] seat()
+	public void seatPersons()
 	{
-		
+		for ()
 	}
 }
